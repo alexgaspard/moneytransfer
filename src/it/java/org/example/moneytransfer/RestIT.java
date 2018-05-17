2c909a6c1bc966a65543d106c3fa5e9daa604a2c
Alexandre Gaspard <alexgaspard@gmail.com>
2018-05-17 16:46:14 +0200

    [add] Adds dependencies for maven-surefire-plugin and leaves only RestIT as integration test

--- src/it/java/org/example/moneytransfer/RestIT.java
+++ src/it/java/org/example/moneytransfer/RestIT.java
@@ -1,4 +1,4 @@
-package org.example.moneytransfer.rest;
+package org.example.moneytransfer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
